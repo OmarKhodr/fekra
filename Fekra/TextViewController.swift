f88a95d40847c0ef4573a7428799e450f24e9557
Omar Khodr <omar.khoder2000@gmail.com>
2021-02-05 22:36:30 +0300

    Add text view

--- Fekra/TextViewController.swift
+++ Fekra/TextViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class TextViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+    
+    let textView = UITextView()
+    
+    override func loadView() {
+        super.loadView()
+        configureTextView()
     }
-
     
 }
 
+extension TextViewController {
+    private func configureTextView() {
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .systemGray6
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = 32
+        
+        view.addSubview(textView)
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+}
