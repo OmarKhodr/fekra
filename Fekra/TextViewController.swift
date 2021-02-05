758a15c6e57a15f1bc1532bf15c154af45580439
Omar Khodr <omar.khoder2000@gmail.com>
2021-02-05 22:40:32 +0300

    Add text content insets

--- Fekra/TextViewController.swift
+++ Fekra/TextViewController.swift
@@ -21,10 +21,20 @@ class TextViewController: UIViewController {
 
 extension TextViewController {
     private func configureTextView() {
+        // Do not create constraints automatically from autoresizing mask, set them up manually
         textView.translatesAutoresizingMaskIntoConstraints = false
+        
         textView.backgroundColor = .systemGray6
+        
+        // Corner radius
         textView.layer.masksToBounds = true
         textView.layer.cornerRadius = 32
+        
+        // Padding for the text inside the view
+        textView.contentInset = UIEdgeInsets(top: 20,
+                                             left: 20,
+                                             bottom: 20,
+                                             right: 20)
         
         view.addSubview(textView)
         NSLayoutConstraint.activate([
