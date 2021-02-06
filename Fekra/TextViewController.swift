a82235982f726df306fed9e7d3645c1a11eaec7e
Omar Khodr <omar.khoder2000@gmail.com>
2021-02-06 08:51:35 +0300

    Add collection view configuration

--- Fekra/TextViewController.swift
+++ Fekra/TextViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class TextViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    
+    override func loadView() {
+        super.loadView()
+        configureHierarchy()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,4 +22,28 @@ class TextViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+}
+
+
+// MARK: Hierarchy
+extension TextViewController {
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+// MARK: Collection View Compositional Layout
+extension TextViewController {
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        
+    }
 }
