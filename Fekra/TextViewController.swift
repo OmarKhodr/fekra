78919bcf6a4222a6f0ae55d1d030f174d0e34762
Omar Khodr <omar.khoder2000@gmail.com>
2021-02-06 08:41:40 +0300

    Create text view

---
+++ Fekra/TextViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TextViewController.swift
+//  Fekra
+//
+//  Created by Omar Khodr on 2/6/21.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
